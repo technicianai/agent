18eb8d630854b2b1c9969ebad01571f79a26b17f
Alec Bell <alec.timothy.bell@gmail.com>
2023-01-20 18:31:30 -0800

    Remove info logs

--- src/woeden_agent/woeden_agent/trigger_worker.py
+++ src/woeden_agent/woeden_agent/trigger_worker.py
@@ -130,12 +130,10 @@ class WoedenTriggerWorker(Node):
         module = importlib.import_module(f"rosbags.typesys.types")
         if hasattr(module, msgtype):
             trigger_cls = getattr(module, msgtype)
-            self.get_logger().info(msgtype)
         else:
             register_types(get_types_from_msg(msgdef, f'woeden_msgs/msg/Trigger{trigger_id}'))
             module = importlib.import_module(f"rosbags.typesys.types")
             trigger_cls =  getattr(module, f"woeden_msgs__msg__Trigger{trigger_id}")
-            self.get_logger().info(msgtype)
         return trigger_cls
 
     def add_handler(self, topic, trigger, msg_cls):
